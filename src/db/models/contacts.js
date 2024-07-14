ef48d6dcb6316d1e6a8a004ea2ed730cfe513015
Alexaanaax <149320517+Alexaanaax@users.noreply.github.com>
2024-07-14 22:26:17 +0200

    add

---
+++ src/db/models/contacts.js
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      default: null,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['home', 'personal'],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
